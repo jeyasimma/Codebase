1302e6980a676d24dea972ba0e9d9b26ad6cdffd
RohitRahulDey <gamergenix0987@gmail.com>
2024-11-04 03:07:25 +0530

    User register, login and logout done  in /Auth; with a /dashbord admin access to manage users

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,37 @@
-console.log("hello ");      
+import express from 'express'
+import dotenv from 'dotenv'
+import DBCon from './libs/db.js'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import AuthRoutes from './routes/Auth.js'
+// import UserRoutes from './routes/user.js'
+import DashboardRoutes from './routes/DashBoard.js'
+// import CommentRoutes from './routes/Comments.js'
+// import PublicRoutes from './routes/Public.js'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 4000
+const app = express()
+DBCon()
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('hello from server')
+})
+app.use(express.static('public'))
+app.use(cookieParser())
+const corsOptoins = {
+    origin: true,
+    credentials: true
+}
+app.use(cors(corsOptoins))
+
+app.use('/auth', AuthRoutes)
+// app.use('/user', UserRoutes)
+app.use('/dashboard', DashboardRoutes)
+// app.use('/comment', CommentRoutes)
+// app.use('/public', PublicRoutes)
+
+app.listen(PORT, () => {
+    console.log(`App is running on Port ${PORT}`)
+})
---
+++ backend/routes/DashBoard.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { isAdmin } from '../middleware/checkAdmin.js'
+// import { Dashboard, Delete, GetUsers } from '../controllers/Dashboard.js'
+import { Delete, GetUsers } from '../controllers/Dashboard.js'
+
+const DashboardRoutes = express.Router()
+
+// DashboardRoutes.get('/', isAdmin, Dashboard)
+// DashboardRoutes.get('/users', isAdmin, GetUsers)
+DashboardRoutes.get('/users', GetUsers)
+// DashboardRoutes.delete("/delete/:id", isAdmin, Delete)
+DashboardRoutes.delete("/delete/:id", Delete)
+
+export default DashboardRoutes
