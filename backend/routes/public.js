53d12f260b87b9c63ce3c669cfb099fdcfbc7990
RohitRahulDey <gamergenix0987@gmail.com>
2024-12-08 22:42:50 +0530

    vercel 2

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { GetSingleuser } from '../controllers/public.js'
+import { GetSingleuser } from "../controllers/public.js"
 
 const PublicRoutes = express.Router()
 
